88531820dc21a7f595e6ba7892204a521f52aeec
AndyLi23 <andyliqy@gmail.com>
2021-09-05 21:17:17 -0700

    add grass

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,51 @@
+let level = 1;
+
+//setup
 let canvas = document.getElementById('main');
 let ctx = canvas.getContext('2d');
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+ctx.imageSmoothingEnabled = false;
 
 
-function draw() {
-    let url = "/assets/img/background.png";
+
+//draw background
+if (level == 1) {
     let bgd = new Image();
-    bgd.src = url;
+    bgd.src = "/assets/img/background.png";
+    var hRatio = canvas.width / bgd.width;
+    var vRatio = canvas.height / bgd.height;
+    var ratio = Math.max(hRatio, vRatio);
+
     bgd.addEventListener("load", function () {
-        ctx.drawImage(bgd, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(bgd, 0, 0, bgd.width * ratio, bgd.height * ratio);
     })
 }
+
+
+
+//draw grass
+if (level == 1) {
+    let grs = new Image();
+    grs.src = "/assets/img/grass.png";
+
+    let rt = (canvas.height / 6) / grs.height;
+
+    grs.addEventListener("load", function () {
+        for (let i = 0; i < canvas.width; i += grs.width * rt) {
+            ctx.drawImage(grs, i, canvas.height - grs.height * rt, grs.width * rt, grs.height * rt);
+        }
+    })
+
+}
+
+function update() {
+
+}
+
+
+setInterval(update, 10);
+
+/*window.addEventListener('resize', function(event){
+    draw();
+});*/
