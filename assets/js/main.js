25e5890ecaa50d102a4a84804b585f544615a03c
AndyLi23 <andyliqy@gmail.com>
2022-07-15 08:13:13 -0700

    multi page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+$( window ).resize(function() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 let cur = 1;
 let loop = 0;
+
+function scrollTo() {
+    console.log("hi");
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+}
 
 $(document).keydown(function (e) {
     switch (e.which) {
